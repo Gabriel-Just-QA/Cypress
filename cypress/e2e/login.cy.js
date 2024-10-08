9a44202f013007bb41150cfbbcf68cd4acba7969
gabriel just <gabrieljust123@gmail.com>
2024-10-07 23:29:18 -0300

    a

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,30 @@
 describe('Teste Completo de Fluxo Site Natura', () => {  
+
+  function gerarSenhaAleatoria() {
+    const caracteres = {
+      maiusculas: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      minusculas: 'abcdefghijklmnopqrstuvwxyz',
+      numeros: '0123456789',
+      especiais: '@#!%&*'
+    };
+    let senha = [
+      caracteres.maiusculas[Math.floor(Math.random() * caracteres.maiusculas.length)],
+      caracteres.minusculas[Math.floor(Math.random() * caracteres.minusculas.length)],
+      caracteres.numeros[Math.floor(Math.random() * caracteres.numeros.length)],
+      caracteres.especiais[Math.floor(Math.random() * caracteres.especiais.length)]
+    ];
+    const todos = caracteres.maiusculas + caracteres.minusculas + caracteres.numeros + caracteres.especiais;
+    while (senha.length < 15) {
+      senha.push(todos[Math.floor(Math.random() * todos.length)]);
+    }
+    return senha.sort(() => Math.random() - 0.5).join('');
+  }
+
+
   let dados;
   let consultor = "juanito"
   let nome = "Gabriel"
+
   before(() => {
     cy.fixture('dados-usuario').then((data) => {
       dados = data;
@@ -16,23 +39,22 @@ beforeEach(() => {
     return false; 
   })
 
-  // cy.get('#inputSearchConsultant').type(consultor)
-  // cy.get('#consultantSearch').click()
-  // cy.get('#consultantContact').click()
-  // cy.get('[data-testid="closeButton"]').should('be.visible').click()
-  // cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div > button.jsx-50b7ee36f9fe1d46.btn_container.container__contained').click({ force: true })
-  //        cy.wait(5000)
-
 });
 
 
     it('Fazer Login com dados validos', () => {
-      cy.login(consultor, dados.email, dados.senha)
+      cy.selecionarConsultor(consultor)
+      cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div > button.jsx-50b7ee36f9fe1d46.btn_container.container__contained').click({ force: true })
+
+      cy.login(dados.email, dados.senha)
 
     }) 
 
     it('Testando logout', () => {
-        cy.login(consultor, dados.email, dados.senha)
+      cy.selecionarConsultor(consultor)
+      cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div > button.jsx-50b7ee36f9fe1d46.btn_container.container__contained').click({ force: true })
+
+        cy.login(dados.email, dados.senha)
         cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true })    
         cy.wait(2000)
         cy.get(':nth-child(7) > .sidemenu-list-item-btn > #gtmSidemenuListItemText').click()
@@ -41,78 +63,72 @@ beforeEach(() => {
 
     });
 
-// it.only('Mudar a Senha', () => {
-//   cy.login(consultor, dados.email, dados.senha)
-//   cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true })    
-//   cy.wait(2000)
-//   cy.get(':nth-child(6) > .sidemenu-list-item-btn > #gtmSidemenuListItemText > #gtmSidemenu-list-item-text').click()
-//   cy.get('#password').should('be.visible').type(dados.senha)
-//   cy.get('#newPassword').type(dados.senha2)
-//   cy.get('#confirmPassword').type(dados.senha2)
-//   cy.get('.profile-password-form > .jsx-50b7ee36f9fe1d46').click()
-//   cy.get('body').should('include',"Dados atualizados com sucesso")
-//   cy.get('h3.jsx-e469f5966417f8ce').should('be.visible')
+  it('Mudar a Senha', () => {
+  const novaSenha = gerarSenhaAleatoria();
+  const fixtureName = 'dados-usuario'; // Nome do arquivo fixture
+  cy.log(novaSenha)
+  cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div > button.jsx-50b7ee36f9fe1d46.btn_container.container__contained').click({ force: true })
 
-//   // Volta para a senha antiga
+  cy.login(dados.email, dados.senha)
 
-//   cy.login(consultor, dados.email, dados.senha2)
-//   cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true })    
-//   cy.wait(2000)
-//   cy.get(':nth-child(6) > .sidemenu-list-item-btn > #gtmSidemenuListItemText > #gtmSidemenu-list-item-text').click()
-//   cy.get('#password').should('be.visible').type(dados.senha2)
-//   cy.get('#newPassword').type(dados.senha)
-//   cy.get('#confirmPassword').type(dados.senha)
-//   cy.get('.profile-password-form > .jsx-50b7ee36f9fe1d46').click()
-//   cy.get('body').should('include',"Dados atualizados com sucesso")
+  cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true })    
+  cy.wait(2000)
+  cy.get(':nth-child(6) > .sidemenu-list-item-btn > #gtmSidemenuListItemText > #gtmSidemenu-list-item-text').click()
+  cy.get('#password').should('be.visible').type(dados.senha)
+  cy.get('#newPassword').type(novaSenha)
+  cy.get('#confirmPassword').type(novaSenha)
+  cy.get('.profile-password-form > .jsx-50b7ee36f9fe1d46').click()
+    // Atualiza a senha no objeto `dados` e grava no JSON
+    cy.updateFixture(fixtureName, novaSenha);
 
+    cy.get('.Toastify__toast-body').should('exist')
+  cy.wait(4000)
+  cy.get('h3.jsx-e469f5966417f8ce').should('be.visible')
 
-// });
+  });
 
-it.only('Mudar a Senha', () => {
-  // Login com a senha atual
-  cy.login(consultor, dados.email, dados.senha);
+  it('Seguir compra sem estar logado',()=>{
+
+    cy.selecionarConsultor(consultor)
+
+    cy.get('#gtmNavigationItem a[href="/c/perfumaria"]').click()
+
+// espera 3 sec
+      cy.wait(3000)
+
+// seleciona o primeiro perfume
+
+      cy.get('.h-categoryResults__results .card').first().click()
+   
+// espera 3 sec
+      cy.wait(3000)
+
+// verifica se a página de detalhes do produto foi carregada
+
+      cy.get('.product-detail-banner-container').should('exist')
+
+// espera 3 sec
+      cy.wait(3000)
+
+// adiciona o produto ao carrinho
+
+      cy.get('.jsx-50b7ee36f9fe1d46.btn_container_icon.container__contained').click()
+   
   
-  // Acessa a página de troca de senha
-  cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true });    
-  cy.wait(2000);
-  cy.get(':nth-child(6) > .sidemenu-list-item-btn > #gtmSidemenuListItemText > #gtmSidemenu-list-item-text').click();
+// verifica se a mensagem de sucesso aparece
 
-  // Trocar de senha atual para senha2
-  cy.get('#password').should('be.visible').type(dados.senha);
-  cy.get('#newPassword').type(dados.senha2);
-  cy.get('#confirmPassword').type(dados.senha2);
-  cy.get('.profile-password-form > .jsx-50b7ee36f9fe1d46').click();
-  
-  // Verifica se a troca de senha foi bem-sucedida
-  cy.get('body').should('include', "Dados atualizados com sucesso");
-  cy.get('h3.jsx-e469f5966417f8ce').should('be.visible');
+      cy.contains('Produto adicionado a sacola').should('be.visible')
+   
+    // Clica em Ver minha sacola
 
-  // Trocar de senha2 para senha3
-  cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true });
-  cy.wait(2000);
-  cy.get(':nth-child(6) > .sidemenu-list-item-btn > #gtmSidemenuListItemText > #gtmSidemenu-list-item-text').click();
-  
-  cy.get('#password').should('be.visible').type(dados.senha2);
-  cy.get('#newPassword').type(dados.senha3); // nova senha intermediária
-  cy.get('#confirmPassword').type(dados.senha3);
-  cy.get('.profile-password-form > .jsx-50b7ee36f9fe1d46').click();
-  
-  // Verifica se a troca de senha foi bem-sucedida
-  cy.get('body').should('include', "Dados atualizados com sucesso");
+    cy.get('#__next > div.jsx-4356fd71142b3e9a > div.jsx-4356fd71142b3e9a.drawer.drawer-right.open > div > div.jsx-4356fd71142b3e9a.drawer-scroll-content.display-scroll-content > div.jsx-44dce6d66b50196f.drawer-footer > button').click()
 
-  // Voltar para a senha original
-  cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div:nth-child(1) > div > span.jsx-1681f388cd853621.modal-login-status-text-content').click({ force: true });
-  cy.wait(2000);
-  cy.get(':nth-child(6) > .sidemenu-list-item-btn > #gtmSidemenuListItemText > #gtmSidemenu-list-item-text').click();
-  
-  cy.get('#password').should('be.visible').type(dados.senha3); // usando a senha intermediária
-  cy.get('#newPassword').type(dados.senha);
-  cy.get('#confirmPassword').type(dados.senha);
-  cy.get('.profile-password-form > .jsx-50b7ee36f9fe1d46').click();
-  
-  // Verifica se a troca de senha foi bem-sucedida
-  cy.get('body').should('include', "Dados atualizados com sucesso");
+// Verifica se o botão de compra esta ativo e clica    
+cy.get('#gtmbuyNow').should('be.enabled').click();
+
+// espera 10sec
+    cy.wait(4000)
+    cy.login(dados.email, dados.senha)
+
+  })
 });
-
-
-    })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -38,19 +38,26 @@ Cypress.Commands.add('isVisible', (selector) => {
 
 })
 
-Cypress.Commands.add('login', (consultor, email, senha) => {
+Cypress.Commands.add('login', ( email, senha) => {
 
-    cy.selecionarConsultor(consultor)
     
-    cy.get('#gtmActionsMenuList > div > div > div.jsx-1681f388cd853621.modal-login-status-content > div > button.jsx-50b7ee36f9fe1d46.btn_container.container__contained').click({ force: true })
-           cy.wait(5000)
            cy.get(':nth-child(1) > .input-common > .jsx-425268b939f6000f').type(email)
            cy.get(':nth-child(2) > .input-common > input.jsx-425268b939f6000f').type(senha)
            cy.get('#gtmLogin').click()
-     
            cy.get('.actions-menu-profile-content > .ActionsMenuListButton', { timeout: 15000 })
            .should('not.contain.text', 'Perfil');
               
     cy.log("Login Feito com sucesso")
   });
   
+  // cypress/support/commands.js
+
+Cypress.Commands.add('updateFixture', (fixtureName, newData) => {
+  cy.fixture(fixtureName).then((data) => {
+    // Atualize a senha com a nova
+    data.senha = newData; // Supondo que a chave da senha seja 'senha'
+    
+    // Escreva os dados atualizados de volta no arquivo JSON
+    cy.writeFile(`cypress/fixtures/${fixtureName}.json`, data);
+  });
+});
